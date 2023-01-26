f387861cd7b1a0a527772e88750849fd89af680a
Konstantin Yeftifeyev <zoxo@outlook.com>
2023-01-26 14:20:11 +0200

    fix: Fix Early closed event

--- modal_bottom_sheet/lib/src/bottom_sheet.dart
+++ modal_bottom_sheet/lib/src/bottom_sheet.dart
@@ -14,7 +14,7 @@ const Curve _decelerateEasing = Cubic(0.0, 0.0, 0.2, 1.0);
 const Duration _bottomSheetDuration = Duration(milliseconds: 400);
 const double _minFlingVelocity = 500.0;
 const double _closeProgressThreshold = 0.6;
-const double _willPopThreshold = 0.8;
+const double _willPopThreshold = 0.0;
 
 typedef WidgetWithChildBuilder = Widget Function(
     BuildContext context, Animation<double> animation, Widget child);
